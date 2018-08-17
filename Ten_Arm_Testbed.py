57bbe8cfffb1f8716be06bc16e760c5044a219d4
Nishant Sinha <nishant15066@iiitd.ac.in>
2018-08-18 00:58:44 +0530

    Graph = steps vs optimal action computed

--- Ten_Arm_Testbed.py
+++ Ten_Arm_Testbed.py
@@ -26,16 +26,13 @@ class Greedy:
         self.q_estimate = np.zeros(k)
         self.action_count = np.zeros(k)
         self.reward = np.zeros(steps)
+        self.correct_action = np.zeros(steps)
 
     def solve(self):
-        # greedily select the arm with biggest reward
         for i in range(steps):
-            greedy_action = 0
-            greedy_reward = -1
-            for j in range(k):
-                if self.q_estimate[j] > greedy_reward:
-                    greedy_action = j
-                    greedy_reward = self.q_estimate[j]
+
+            # greedily select the arm with biggest reward
+            greedy_action = np.argmax(self.q_estimate)
 
             # Get reward from the greedily selected arm
             actual_reward = get_reward(self.q_star, greedy_action)
@@ -46,6 +43,10 @@ class Greedy:
             #updating reward at this step
             self.reward[i] = actual_reward
 
+            #checking if it was the optimal reward
+            if greedy_action == np.argmax( self.q_star ):
+                self.correct_action[i] = 1
+
     def get_q_estimate(self):
         print(self.action_count)
         return self.q_estimate
@@ -53,6 +54,8 @@ class Greedy:
     def get_reward(self):
         return self.reward
 
+    def get_correct_action_count(self):
+        return self.correct_action
 
 # Solves one k-arm bandit problem using epsilon-greedy approach
 class Epsilon_greedy:
@@ -63,23 +66,23 @@ class Epsilon_greedy:
         self.action_count = np.zeros(k)
         self.reward = np.zeros(steps)
         self.epsilon = epsilon
+        self.correct_action = np.zeros(steps)
 
     def solve(self):
         # Epsilon greedy Method
         for i in range(steps):
-            greedy_action = 0
-            greedy_reward = -1
-
             random_variable = np.random.uniform(0,1)
+
+            #explore
             if( random_variable <= self.epsilon ):
                 greedy_action = np.random.uniform(0,1)
                 greedy_action *= 10
                 greedy_action = int(greedy_action)
+
+            #exploit
             else:
-                for j in range(k):
-                    if self.q_estimate[j] > greedy_reward:
-                        greedy_action = j
-                        greedy_reward = self.q_estimate[j]
+                # greedily select the arm with biggest reward
+                greedy_action = np.argmax(self.q_estimate)
 
             # Get reward from the (not so)greedily selected arm
             actual_reward = get_reward(self.q_star, greedy_action)
@@ -90,6 +93,10 @@ class Epsilon_greedy:
             #updating reward at this step
             self.reward[i] = actual_reward
 
+            #checking if it was the optimal reward
+            if greedy_action == np.argmax( self.q_star ):
+                self.correct_action[i] = 1
+
     def get_q_estimate(self):
         print(self.action_count)
         return self.q_estimate
@@ -97,6 +104,22 @@ class Epsilon_greedy:
     def get_reward(self):
         return self.reward
 
+    def get_correct_action_count(self):
+        return self.correct_action
+
+def create_objects():
+    q_star = np.random.normal(0, variance, 10)
+
+    greedy_solution = Greedy(q_star)
+    greedy_solution.solve()
+
+    epsilon_1 = Epsilon_greedy(q_star, 0.1)
+    epsilon_1.solve()
+
+    epsilon_01 = Epsilon_greedy(q_star, 0.01)
+    epsilon_01.solve()
+
+    return greedy_solution, epsilon_1, epsilon_01
 
 def steps_vs_average_reward():
     avg_reward_greedy = np.zeros(steps)
@@ -104,18 +127,10 @@ def steps_vs_average_reward():
     avg_reward_epsilon_01 = np.zeros(steps)
 
     for run in range(iters):
-        q_star = np.random.normal(0, variance, 10)
+        greedy_solution, epsilon_1, epsilon_01 = create_objects()
 
-        greedy_solution = Greedy(q_star)
-        greedy_solution.solve()
         avg_reward_greedy = np.add(avg_reward_greedy, greedy_solution.get_reward())
-
-        epsilon_1 = Epsilon_greedy(q_star, 0.1)
-        epsilon_1.solve()
         avg_reward_epsilon_1 = np.add(avg_reward_epsilon_1, epsilon_1.get_reward())
-
-        epsilon_01 = Epsilon_greedy(q_star, 0.01)
-        epsilon_01.solve()
         avg_reward_epsilon_01 = np.add(avg_reward_epsilon_01, epsilon_01.get_reward())
 
     avg_reward_greedy = np.divide(avg_reward_greedy, iters)
@@ -125,5 +140,23 @@ def steps_vs_average_reward():
     plt.plot(avg_reward_greedy, 'r', avg_reward_epsilon_1, 'b', avg_reward_epsilon_01, 'g')
     plt.show()
 
+def steps_vs_optimal_action():
+    optimal_action_greedy = np.zeros(steps)
+    optimal_action_epsilon_1 = np.zeros(steps)
+    optimal_action_epsilon_01 = np.zeros(steps)
 
-steps_vs_average_reward()
+    for run in range(iters):
+        greedy_solution, epsilon_1, epsilon_01 = create_objects()
+
+        optimal_action_greedy = np.add( optimal_action_greedy, greedy_solution.get_correct_action_count() )
+        optimal_action_epsilon_1 = np.add( optimal_action_epsilon_1, epsilon_1.get_correct_action_count() )
+        optimal_action_epsilon_01 = np.add( optimal_action_epsilon_01, epsilon_01.get_correct_action_count() )
+
+    optimal_action_greedy = np.divide(optimal_action_greedy, iters)
+    optimal_action_epsilon_1 = np.divide(optimal_action_epsilon_1, iters)
+    optimal_action_epsilon_01 = np.divide(optimal_action_epsilon_01, iters)
+
+    plt.plot(optimal_action_greedy, 'r', optimal_action_epsilon_1, 'b', optimal_action_epsilon_01, 'g')
+    plt.show()
+
+steps_vs_optimal_action()
